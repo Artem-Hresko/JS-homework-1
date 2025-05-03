1ce40436f3b26a8809ed53dd04ff1745fc819241
Artem Hresko <gresko.artem09@gmail.com>
2025-05-03 22:26:06 +0100

    /

---
+++ js/index.js
@@ -0,0 +1,48 @@
+const age = 16;
+
+console.log("value of variable age :", age);
+
+const name = "Artem";
+
+console.log("value of variable name :", name);
+
+const isStudent = true;
+
+console.log("value of variable isStudent :", isStudent);
+
+const myString =
+  "You have to fight to reach your dream. You have to sacrifice and work hard for it.";
+
+console.log("value of variable myString :", myString);
+
+const myNumber = 5;
+
+console.log("value of variable myNumber :", myNumber);
+
+let Result = myNumber + 10;
+
+console.log("value of variable Result :", Result);
+
+const myNull = "Null";
+
+console.log("value of variable  myNull :", myNull);
+
+let userName = prompt("Введіть своє ім’я");
+
+alert("Ваше ім’я: " + userName);
+
+console.log("Тип змінної userName:", typeof userName);
+
+const message = "Ви дійсно хочете покинути сторінку?";
+
+const userAnswer = window.confirm(message);
+
+console.log("Тип змінної userAnswer:", typeof userAnswer);
+
+const alertMessage = "Увага! Дія може бути небезпечною.";
+
+alert(alertMessage);
+
+const confirmation = "Ви впевнені, що хочете продовжити?";
+
+const userAnswer2 = window.confirm(confirmation);
